ba0f67ecbf5f8ce68cb3dfb1a59d82e577516f10
Murray Lee <murray.lee@beckietech.com>
2012-07-04 15:36:04 +0800

    copy buf-s1 to buf-s2

---
+++ t3.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+
+int main()
+{
+    char s1[] = "hello, world!";
+    char s2[80] = "hi, kitty";
+    char *p1, *p2;
+
+	printf("s1 = %s\ns2 = %s\n", s1, s2);
+
+	p1 = s1, p2 = s2;
+	while (*p1 != '\0')	{
+		if (*p1 == ' ')	{
+			*p2++ = '-';
+			*p2 = '-';
+		} else
+			*p2 = *p1;
+
+		p1++, p2++;
+	}
+
+	printf("s1 = %s\ns2 = %s\n", s1, s2);
+
+    return 0;
+}
