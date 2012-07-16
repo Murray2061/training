ef0057bee3ac6411fccdaea74a6b563b786e720e
Phil Huang <phil.huang@beckietech.com>
2012-07-16 16:47:28 +0800

    reversal of words

--- reversalline.c
+++ reversalline.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main()
 {
     char	str[1024], temp;
-    char	*p1, *p2,*pend,*pnow;
-    int  n, i;
+    char	*p1, *p2, *pend, *pnow;
     FILE	*rptr;
 
     if ((rptr = fopen("fread.man", "r")) == NULL) {
@@ -16,38 +16,54 @@ int main()
             fgets(str, sizeof (str), rptr);
             pnow = str;
             pend = str;
-			p1=str;
-			p2=str;
-			while (*pend++){}
-			pend -=2;
-			if(*pend=='\n'){pend--;}
-			while(pend-p1>=0){
-			if(isspace(*p2)){
-			while(isspace(*p2++)){}
-			p2--;
-			if(*p2=='\0'){p2--;}
-			if(*p2=='\n'){p2--;}
-			if(isspace(*p2)){p2++;}
-			p1=p2;
-			}
-			
-			while (!isspace(*p2++)) {}
-            p2 --;
-            if (*p2 == '\n') {
-                p2--;
+            p1 = str;
+            p2 = str;
+            while (*pend++) {
             }
-			pnow=p2;
-            while (p2 - p1 > 0) {
-                temp = *p1;
-                *p1 = *p2;
-                *p2 = temp;
-                p2--;
-                p1++;
+            pend -= 2;
+            if (*pend == '\n') {
+                pend--;
             }
-			
-			p1=pnow;
-            p2=pnow;
-             }
+            while (pend - p1 >= 0) {
+                if (isspace(*pnow)) {
+                    while (isspace(*pnow++)) {
+                    }
+                    pnow--;
+                    if (*pnow == '\0') {
+                        pnow--;
+                    }
+                    if (*pnow == '\n') {
+                        pnow--;
+                    }
+                    if (isspace(*pnow)) {
+                        pnow++;
+                    }
+                    p2 = pnow;
+                    p1 = p2;
+                }
+                if (*pnow == '\n') {
+                    break;
+                }
+                while (!isspace(*p2++)) {
+                }
+                p2--;
+                if (*p2 == '\n') {
+                    p2--;
+                }
+                pnow = p2 + 1;
+                while (p2 - p1 > 0) {
+                    temp = *p1;
+                    *p1 = *p2;
+                    *p2 = temp;
+                    p2--;
+                    p1++;
+                }
+                if (*pnow == '\0' || *pnow == '\n') {
+                    break;
+                }
+                p1 = pnow;
+                p2 = pnow;
+            }
             fprintf(stdout, "%s", str);
         }
     }
