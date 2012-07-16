d9d43f642f0ecf135c2e2a4c9746bc65cc5d457f
Phil Huang <phil.huang@beckietech.com>
2012-07-16 16:31:36 +0800

    reversal of inline words

--- reversalline.c
+++ reversalline.c
@@ -4,8 +4,8 @@
 int main()
 {
     char	str[1024], temp;
-    char	*p1, *p2;
-    int		bn, i;
+    char	*p1, *p2,*pend,*pnow;
+    int  n, i;
     FILE	*rptr;
 
     if ((rptr = fopen("fread.man", "r")) == NULL) {
@@ -14,14 +14,29 @@ int main()
     }else {
         while (!feof(rptr)) {
             fgets(str, sizeof (str), rptr);
-            p1 = str;
-            p2 = str;
-            while (*p2++) {
-            }
-            p2 -= 2;
+            pnow = str;
+            pend = str;
+			p1=str;
+			p2=str;
+			while (*pend++){}
+			pend -=2;
+			if(*pend=='\n'){pend--;}
+			while(pend-p1>=0){
+			if(isspace(*p2)){
+			while(isspace(*p2++)){}
+			p2--;
+			if(*p2=='\0'){p2--;}
+			if(*p2=='\n'){p2--;}
+			if(isspace(*p2)){p2++;}
+			p1=p2;
+			}
+			
+			while (!isspace(*p2++)) {}
+            p2 --;
             if (*p2 == '\n') {
                 p2--;
             }
+			pnow=p2;
             while (p2 - p1 > 0) {
                 temp = *p1;
                 *p1 = *p2;
@@ -29,7 +44,10 @@ int main()
                 p2--;
                 p1++;
             }
-
+			
+			p1=pnow;
+            p2=pnow;
+             }
             fprintf(stdout, "%s", str);
         }
     }
