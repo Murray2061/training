3fff0bd33d11f930eb12a140bbe4b0edce481e5b
Phil Huang <phil.huang@beckietech.com>
2012-07-16 18:19:08 +0800

    write to file for capitalization

--- filerw2.c
+++ filerw2.c
@@ -7,24 +7,34 @@
 
 int main()
 {
-    char str[1024];
-    int	fd,bn,i;
-    
-    if ((fd = open("fread.man",O_RDONLY,0644)) == -1) {
-        printf("File could not be opened");
+    char	str[1024];
+    int		fd, fd2, bn, i;
+
+    if ((fd2 = open("fread3.man", O_WRONLY | O_CREAT, 0644)) == -1) {
+        printf("Write file could not be opened");
+        exit(1);
+    }
+
+    if ((fd = open("fread.man", O_RDONLY, 0644)) == -1) {
+        printf("Read file could not be opened");
+        exit(1);
+
     }else {
-        while ((bn=read(fd, str, sizeof(str)))>0) {
-             for (i = 0; i < bn; i++) {
+        while ((bn = read(fd, str, sizeof (str))) > 0) {
+            for (i = 0; i < bn; i++) {
                 if (str[i] >= 'a' && str[i] <= 'z') {
                     str[i] = str[i] - 32;
                 }
             }
-            write(1,str,bn);
+            write(fd2, str, bn);
         }
 
-		}
+    }
 
     close(fd);
+    close(fd2);
 
     return 0;
 }
+
+
