74605687b8db533b1a219888ce01d03d9101b666
Murray Lee <murray.lee@beckietech.com>
2012-07-16 12:36:03 +0800

    fix bug in filerw.c

--- filerw.c
+++ filerw.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
+# include <stdlib.h>
+
 int main()
 {
     char	str[80];
-    int	bn;
+    int		bn;
     FILE	*rptr;
 
-//FILE *wptr;
-//wptr=fopen("phil2.txt","w");
     if ((rptr = fopen("fread.man", "r")) == NULL) {
-        printf("File could not be opened");
-    }else {
-        while (!feof(rptr)) {
-            bn = fread(str, sizeof (str), 1, rptr);
-            fwrite(str, bn, 1, stdout);
-//fwrite(str,sizeof(str),1,wptr);
-        }
+        perror("fopen");
+		exit(1);
     }
+
+	while (!feof(rptr)) {
+           bn = fread(str, 1, sizeof (str), rptr);
+//		   printf("bn = %d\n", bn);
+           fwrite(str, 1, bn, stdout);
+	}
+
     fclose(rptr);
+
+	return 0;
 }
 
 
